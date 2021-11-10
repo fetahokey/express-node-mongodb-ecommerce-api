3ab94f0b14e25bb82986924964b06ec64d1974c0
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-10 20:06:34 +0000

    added `auth` route

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./connect");
 const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+const { json, urlencoded } = express;
 const app = express();
-
-app.use("/users", userRouter);
 
 dotenv.config();
 connectDB();
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
 
 app.listen(5000, () => {
   console.log("Backend server is running ..");
