13a0800f4bef61536da85b458dc85299c265695a
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-12 22:32:19 +0000

    add `login` funciton to the auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
-const CryptoJS = require('crypto-js');
+const CryptoJS = require("crypto-js");
 
 exports.registerUser = asyncHandler(async (req, res, next) => {
   const { username, email, password, isAdmin } = req.body;
@@ -15,6 +15,26 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
   const savedUser = await newUser.save();
   if (savedUser) {
     res.status(201).json({ user: savedUser });
+  } else {
+    res.status(500).json({ error: "something went worng! " });
+  }
+});
+
+exports.loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({
+    email,
+  });
+  if (!user ) return res.status(401).json({error: 'Bad Credentials!'});
+
+  const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.JWT_SECRET);
+  const userPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+  
+  if (userPassword !== password) return res.status(401).json({error: 'Bad Credentials!'});
+
+  if (user) {
+    res.status(200).json({ user });
   } else {
     res.status(500).json({ error: "something went worng! " });
   }
