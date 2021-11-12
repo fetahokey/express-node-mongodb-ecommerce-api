ca67243f8c0f74e6bfd8b1db08b11a94a0914c3e
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-12 22:40:23 +0000

    omit the `password` form the 200 retrun

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,20 +21,26 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
 });
 
 exports.loginUser = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, ...password_ } = req.body;
 
   const user = await User.findOne({
     email,
   });
-  if (!user ) return res.status(401).json({error: 'Bad Credentials!'});
+  if (!user) return res.status(401).json({ error: "Bad Credentials! 0" });
 
-  const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.JWT_SECRET);
+  const hashedPassword = CryptoJS.AES.decrypt(
+    user.password,
+    process.env.JWT_SECRET
+  );
   const userPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-  
-  if (userPassword !== password) return res.status(401).json({error: 'Bad Credentials!'});
+
+  if (userPassword !== password_.password)
+    return res.status(401).json({ error: "Bad Credentials! 1" });
+
+  const { password, ...others } = user._doc;
 
   if (user) {
-    res.status(200).json({ user });
+    res.status(200).json({ user: others });
   } else {
     res.status(500).json({ error: "something went worng! " });
   }
