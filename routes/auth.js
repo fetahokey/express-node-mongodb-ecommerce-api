b3f91beb0d97fb28e7c8c68740197a65cc972d44
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-12 22:32:29 +0000

    add `login` funciton to the auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
-const { registerUser } = require("../controllers/auth");
+const { registerUser, loginUser } = require("../controllers/auth");
 const { validateRegisterUser } = require("../validators/auth");
 
 // register user
 router.post("/", validateRegisterUser, registerUser);
 
+// load user
 //router.get("/", getUsers);
 
 // authenticate user
+router.post("/login", loginUser);
 
 module.exports = router;
