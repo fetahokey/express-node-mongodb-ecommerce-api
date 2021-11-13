a013ac7a58bcdd254e1b08325539d1f8ff72a3e7
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-13 22:54:52 +0000

    add `getAllProducts` route

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,17 @@
-const { createProduct } = require("../controllers/product");
+const {
+  createProduct,
+  getAllProducts,
+  updateProduct,
+} = require("../controllers/product");
 const { verifyTokenAndAdmin } = require("../utils/verifyToken");
 const { validateCreateProduct } = require("../validators/product");
 
 const router = require("express").Router();
 
 router.post("/", verifyTokenAndAdmin, validateCreateProduct, createProduct);
+
+router.get("/", verifyTokenAndAdmin, getAllProducts);
+
+router.put("/:id", verifyTokenAndAdmin, updateProduct);
 
 module.exports = router;
