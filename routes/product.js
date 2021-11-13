840882dfcb670bdaead5794c2c1cae87f1170559
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-13 22:42:36 +0000

    add `createProduct` route

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,9 @@
+const { createProduct } = require("../controllers/product");
+const { verifyTokenAndAdmin } = require("../utils/verifyToken");
+const { validateCreateProduct } = require("../validators/product");
+
 const router = require("express").Router();
+
+router.post("/", verifyTokenAndAdmin, validateCreateProduct, createProduct);
 
 module.exports = router;
