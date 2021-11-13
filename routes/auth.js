a06b641f2336cb02f261093baecd2704de666b8c
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-13 21:28:48 +0000

    add `delete` `get` routes for the auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,28 @@
 const router = require("express").Router();
-const { registerUser, loginUser, updateUser } = require("../controllers/auth");
-const { verifyTokenAndAuthorization } = require("../utils/verifyToken");
+const {
+  registerUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getUser,
+} = require("../controllers/auth");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("../utils/verifyToken");
 const { validateRegisterUser } = require("../validators/auth");
 
 // register user
 router.post("/", validateRegisterUser, registerUser);
 
 // load user
-//router.get("/", getUsers);
+router.get("/:id", verifyTokenAndAdmin, getUser);
 
 // update user
 router.put("/:id", verifyTokenAndAuthorization, updateUser);
+
+// delete user
+router.delete("/:id", verifyTokenAndAuthorization, deleteUser);
 
 // authenticate user
 router.post("/login", loginUser);
