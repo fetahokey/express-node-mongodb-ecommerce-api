93287af0eeaad09ed38e0937d355cfe7046ac552
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-13 22:55:23 +0000

    add `getAllProducts`  function to the product controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,41 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/Product");
 
+// @route POST /products
+// @desc create new product
+// @access Private
 exports.createProduct = asyncHandler(async (req, res, next) => {
   const newProduct = new Product(req.body);
   const savedProduct = await newProduct.save();
   if (savedProduct) {
     return res.status(201).json({ data: savedProduct });
+  } else {
+    return res.status(500).json({ error: "something went worng!" });
+  }
+});
+
+// @route GET /products
+// @desc get all products
+// @access Private
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+
+  if (products) {
+    return res.status(201).json({ data: products });
+  } else {
+    return res.status(500).json({ error: "something went worng!" });
+  }
+});
+
+// @route PUT /products/:id
+// @desc update product
+// @access Private
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  const productId = req.params.id;
+  const updatedProduct = await Product.findByIdAndUpdate(productId);
+
+  if (updatedProduct) {
+    return res.status(201).json({ data: updatedProduct });
   } else {
     return res.status(500).json({ error: "something went worng!" });
   }
